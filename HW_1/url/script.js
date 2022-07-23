da26c146f4551af8d8f8ae997c76fa2ea300dd6e
Yaromir Taraskin <62649776+YaromirTaraskin@users.noreply.github.com>
2022-07-23 07:26:34 +0300

    URL part done

--- HW_1/url/script.js
+++ HW_1/url/script.js
@@ -1,16 +1,26 @@
-document.write("<p>" + "The HREF of the document is "+ document.location.href+ "</p>");
+//Excerpting and showing URLA
+document.write("<p>" + "The Uniform Resource Locators address of the document is "+ document.location.href+ "</p>");
 document.write("<p>" + "The excerpted protocol is " + document.location.protocol+ "</p>");
 
-var pathname = document.location.pathname
+//Excerpting and showing pathname
+const pathname = document.location.pathname;
 document.write("<p>" + "The full excerpted filename is " + pathname + "</p>");
 
-var indexFilenameStarts = pathname.lastIndexOf("/") + 1;
-var filename = pathname.substr(indexFilenameStarts);
+//Excerpting and showing short filename
+const indexFilenameStarts = pathname.lastIndexOf("/") + 1;
+const filename = pathname.substr(indexFilenameStarts);
 document.write("<p>" + "The short excerpted filename is " + filename + "</p>");
 
-var indexExtensionStarts = filename.lastIndexOf(".") + 1;
-var extension_name = filename.substr(indexExtensionStarts);
+//Excerpting and showing extension
+const indexExtensionStarts = filename.lastIndexOf(".") + 1;
+const extension_name = filename.substr(indexExtensionStarts);
 document.write("<p>" + "The excerpted extension of the file is " + extension_name + "</p>");
 
-var paramsString = document.location.search;
-
+//Excerpting substring of params and creating the required object of them
+const paramsString = document.location.search;
+const searchParams = new URLSearchParams(paramsString);
+const paramsObject = new Object();
+for(const pair of searchParams.entries()) {
+    paramsObject[pair[0]] = pair[1];
+}
+document.write("<p>Params object has been created.</p>");
